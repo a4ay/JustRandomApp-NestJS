f968e1284dc3ff4f8a9b5328a2247f8c16ed8df9
Azay Pdn <azaypdn@gmail.com>
2023-02-04 12:15:54 +0000

    feat: added jwt functionality

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Request, Get, UseGuards, Post } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req: any) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
