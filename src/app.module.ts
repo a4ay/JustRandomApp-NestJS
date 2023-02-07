80eab46efb850a09f552cf674d4ad35940863f72
Ajay Pradhan <azaypdn@gmail.com>
2023-02-07 23:09:36 +0530

    feat: created typeorm config service file with config service i.e env var

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,23 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DBConfigService } from './DBConfigService';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 
 @Module({
-  imports: [UsersModule, AuthModule, ConfigModule.forRoot()],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.development.env'
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DBConfigService,
+      inject: [DBConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DBConfigService],
 })
 export class AppModule {}
